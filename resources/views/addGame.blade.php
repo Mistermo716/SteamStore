cf2e2bf99ef694442d3d3ad86604b89069141d5a
mistermo716 <Muaath.Alaraj@gmail.com>
2018-11-29 15:14:26 -0600

    addGame form

--- resources/views/addGame.blade.php
+++ resources/views/addGame.blade.php
@@ -5,8 +5,18 @@
     {{csrf_field()}}
     <label>Name</label> <br>
     <input name="name" type="text" placeholder="Name of Game"></input> <br> <br>
+    <label>Publisher</label> <br>
     <input name="publisher" type="text" placeholder="Publisher"></input> <br> <br>
+    <label>Score</label> <br>
     <input name="score" type="text" placeholder="Score"></input> <br> <br>
-    <input type="submit"></input>
+    <label>Votes</label> <br>
+    <input name="votes" type="text" placeholder="Votes"></input> <br> <br>
+    <label>Price</label> <br>
+    <input name="price" type="text" placeholder="Price"></input> <br> <br>
+    <label>Image URL</label> <br>
+    <input name="img_url" type="text" placeholder="Image URL"></input> <br> <br>
+    <label>Description</label> <br>
+    <input name="description" type="textarea" placeholder="Description"></input> <br> <br>
+    <input class="btn btn-success" type="submit"></input>
 </form>
 @endsection
