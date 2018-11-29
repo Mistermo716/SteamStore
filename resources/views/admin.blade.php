d152bc0ecbf1bc71032a5a019c0f39b0a254403f
mistermo716 <Muaath.Alaraj@gmail.com>
2018-11-29 10:23:51 -0600

    admin product table

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,12 +1,42 @@
 @extends('layouts.main')
 
 @section('content')
+<script>
+    function confirmDelete(){
+      return confirm("Are you sure you want to delete this product?");        
+    }
+    
+  </script>
     <h1 class="text-center">
         Hello {{$admin}}
     </h1>
-    <ul>
+    <table class='table table-hover'>
+        <thead>
+                <tr>
+                  <th scope='col'>ID</th>
+                  <th scope='col'>Name</th>
+                  <th scope='col'>Publisher</th>
+                  <th scope='col'>Genre</th>
+                  <th scope='col'>Score</th>
+                  <th scope='col'>Price</th>
+                  <th scope='col'>Update</th>
+                  <th scope='col'>Remove</th>
+                  </tr>
+        </thead>
         @foreach($games as $game)
-            <li>{{$game->name}}</li>
+        <tr>
+            <td>{{$game->id}}</td>
+            <td>{{$game->name}}</td>
+            <td>{{$game->publisher}}</td>
+            <td>{{$game->genre->name}}</td>
+            <td>{{$game->score}}</td>
+            <td>${{$game->price}}</td>
+        <td><a class='btn btn-primary' href='updateProduct.php?productId="{{$game->id}}"'>Update</a></td>
+            <form onsubmit='return confirmDelete()'>
+            <input type='hidden' name='productId' value="{{$game->id}}" />
+            <td><input type='submit' class='btn btn-danger' value='Remove'></td>
+            </form>
+        </tr>
         @endforeach
-    </ul>
+    </table>
 @endsection
