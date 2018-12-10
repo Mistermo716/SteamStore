169db612fc6d3801850fef161a48908fe62686d3
mistermo716 <Muaath.Alaraj@gmail.com>
2018-12-10 12:50:37 -0600

    add script to admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.base', ['title' => 'Admin'])
 
 @section('body')
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.js"></script>
     <nav class="navbar navbar-light navbar-expand-lg bg-dark sticky-top">
         <a class="navbar-brand" href="{{ url('') }}">
